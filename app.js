c8e1a054a20405463637764a3d5509e30079b817
misun <eating8@naver.com>
2021-11-05 02:00:33 +0900

    저장 기능 구현

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const fill = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 const init_color = "#2c2c2c";
 
 canvas.width = 700;
 canvas.height = 700;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = init_color;
 ctx.lineWidth = 2.5;
-ctx.fillStyle = init_color;
-
 
 let painting = false;
 
@@ -34,11 +35,20 @@ function stopPainting() {
     painting = false;
 }
 
+function handleCM(event) {
+    event.preventDefault()
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+function handleFillColor() {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function handleColorClick(event) {
@@ -52,8 +62,12 @@ function handleRangeChange(event) {
     ctx.lineWidth = size;
 }
 
-function handleFillColor() {
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+function handleSaveClick() {
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image
+    link.download = "코딩소녀의 바닐라JS 그림판 ^^"
+    link.click();
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
@@ -64,4 +78,8 @@ if (range) {
 
 if (fill) {
     fill.addEventListener("click", handleFillColor)
+}
+
+if (save) {
+    save.addEventListener("click", handleSaveClick)
 }
