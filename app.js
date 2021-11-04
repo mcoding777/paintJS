aa729e29a06bef0c20d18874e6ec249d6c9621c5
misun <eating8@naver.com>
2021-11-05 00:43:15 +0900

    fill 기능 구현!!!

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const fill = document.getElementById("jsMode");
+const init_color = "#2c2c2c";
 
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = init_color;
 ctx.lineWidth = 2.5;
+ctx.fillStyle = init_color;
+
 
 let painting = false;
 
@@ -16,7 +20,6 @@ function onMouseMove(event) {
     const y = event.offsetY;
     if (!painting) {
         ctx.beginPath();
-        ctx.moveTo(x, y);
     } else {
         ctx.lineTo(x, y);
         ctx.stroke();
@@ -41,6 +44,7 @@ if (canvas) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -48,8 +52,16 @@ function handleRangeChange(event) {
     ctx.lineWidth = size;
 }
 
+function handleFillColor() {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
 
 if (range) {
     range.addEventListener("input", handleRangeChange)
+}
+
+if (fill) {
+    fill.addEventListener("click", handleFillColor)
 }
